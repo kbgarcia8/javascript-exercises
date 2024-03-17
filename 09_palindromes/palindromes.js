5c80c6d4799815108f05c831182593df3ed8eec5
kbgarcia8 <kbgarcia8@users.noreply.github.com>
2024-03-18 01:17:37 +0800

    submitted 03/15/2024

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,51 @@
 const palindromes = function () {
-
+    let inputWord = arguments[0];
+    const regex = /[a-zA-Z0-9]/;
+    const regex1 = /[A-Z]/;
+    const regex2 = /^\S*$/;
+    const hasNoSpace = regex2.test(inputWord);
+    if(arguments.length === 1 && hasNoSpace) {
+      let wordCharacters = inputWord.split('');
+      let capitalLetter = wordCharacters.filter(element => regex1.test(element));
+      let letterandnumOnly = wordCharacters.filter(element => regex.test(element));
+      let punctuations = wordCharacters.filter(element => !regex.test(element));
+      let wordCharactersRev = letterandnumOnly.reverse();
+      if (capitalLetter.length !== 0) {
+        wordCharactersRev[wordCharactersRev.length-1] = wordCharactersRev[wordCharactersRev.length-1].toLowerCase();
+        wordCharactersRev[0] = wordCharactersRev[0].toUpperCase();
+      }
+      let finalRevCharacters = wordCharactersRev.concat(punctuations);
+      let finalPalindrome = finalRevCharacters.join('');
+      console.log(capitalLetter);
+      console.log(inputWord);
+      console.log(wordCharacters);
+      console.log(letterandnumOnly);
+      console.log(punctuations);
+      console.log(wordCharactersRev);
+      console.log(finalRevCharacters);
+      console.log(finalPalindrome);
+      if (inputWord === finalPalindrome) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      let initialInputNoSpace = arguments[0].split(' ').join('').split('');
+      let filteredInput = initialInputNoSpace.filter(element => regex.test(element));
+      let capitalLetter = filteredInput.filter(element => regex1.test(element));
+      let finalInput = filteredInput.join('');
+      let palindromeTest = filteredInput.reverse();
+      if (capitalLetter.length !== 0) {
+        palindromeTest[palindromeTest.length-1] = palindromeTest[palindromeTest.length-1].toLowerCase();
+        palindromeTest[0] = palindromeTest[0].toUpperCase();
+      }
+      let finalPalindrome = palindromeTest.join('');
+      if (finalInput === finalPalindrome) {
+        return true;
+      } else {
+        return false;
+      }
+    }
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
 const fibonacci = function() {
-
+    let input = arguments[0];
+    let fibonacciSet = [];
+    let fibonacciElement = 0;
+    //need to have an array to push and access previous element
+    if(input == 0) {
+        return 0;
+    } else if (input !== 0 && input> 0) {
+        for(let i = 0; i<input; i++) {
+            if (i == 0 || i == 1) {
+            fibonacciElement = 1
+            fibonacciSet.push(fibonacciElement);
+            } else {
+            fibonacciElement = fibonacciSet[i-2] + fibonacciSet[i-1];
+            fibonacciSet.push(fibonacciElement);
+            }
+        }
+        return fibonacciElement;
+    } else if (input !== 0 && input<0) {
+        return "OOPS";
+    }
 };
 
 // Do not edit below this line
