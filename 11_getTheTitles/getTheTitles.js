f79f0c249aef50dacd1f3aa0124e4fa87d2578c4
kbgarcia8 <kbgarcia8@users.noreply.github.com>
2024-03-18 01:33:59 +0800

    updated and solved 03/15/2024

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,18 @@
 const getTheTitles = function() {
-
+    let bookArr = [];
+    const books = [
+        {
+          title: 'Book',
+          author: 'Name'
+        },
+        {
+          title: 'Book2',
+          author: 'Name2'
+        }
+    ];
+    bookArr.push(books[0].title);
+    bookArr.push(books[1].title);
+    return bookArr;
 };
 
 // Do not edit below this line
